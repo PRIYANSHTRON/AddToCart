de6c2bac8d7ee06cf16d19d5f58b55b287bc7429
PRIYANSHTRON <priyanshrusia2002@gmail.com>
2023-05-25 23:44:58 +0530

    style
    Added some styling to the page

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const input_field=document.getElementById('input-field')
 const add_button=document.getElementById('add-button')
 
 add_button.addEventListener('click', function () {
-    alert('got clicked')
+    alert(input_field.value)
 })
