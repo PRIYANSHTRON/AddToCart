032c747ea5afbc635ed19d6fe2d1c13787f5938b
PRIYANSHTRON <priyanshrusia2002@gmail.com>
2023-05-25 18:12:17 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const input_field=document.getElementById('input-field')
+const add_button=document.getElementById('add-button')
+
+add_button.addEventListener('click', function () {
+    alert('got clicked')
+})
