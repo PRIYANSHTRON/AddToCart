21c92fcba028f5ce4392bc087f4b80ae54443e5f
PRIYANSHTRON <priyanshrusia2002@gmail.com>
2023-05-26 22:32:34 +0530

    refactor

--- index.js
+++ index.js
@@ -13,19 +13,36 @@ const database = getDatabase(app)
 
 
 //creating reference in database
-const itemsInDB = ref(database, "items")
+const items = ref(database, "items")
 // argument 1 - name of database in which reference is to be created
 // argument 2 - name of the refrence 
 
 
 const input_field = document.getElementById('input-field')
-const add_button=document.getElementById('add-button')
+const add_button = document.getElementById('add-button')
 
 add_button.addEventListener('click', function () {
     //pushes the value in DB
-    push(itemsInDB ,input_field.value)
-    // argument 1 - reference 
-    // argument 2 - data to be pushed
+    let itemName = input_field.value
     
-    alert(input_field.value)
+    push(items,input_field.value)
+    // argument 1 - reference
+    // argument 2 - data to be pushed
+
+    clearInputField()
+
+    // if (itemName.length == 0)
+    //     alert('error')
+    // else   
+        appendItems(itemName)
+    
+
 })
+
+function clearInputField(){ 
+    input_field.value=""
+ }
+
+function appendItems(itemName) {
+    document.getElementById('list').innerHTML += `<li>${itemName}</li>`
+}
