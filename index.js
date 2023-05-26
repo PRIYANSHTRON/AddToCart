e6431027ff435ec4d317d3663c698ba1e4827068
PRIYANSHTRON <priyanshrusia2002@gmail.com>
2023-05-26 20:04:01 +0530

    feat
    Added Firebase to the project

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-const input_field=document.getElementById('input-field')
+import {initializeApp} from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js'
+import { getDatabase, ref, push } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js'
+
+const appSettings = {
+    databaseURL: "https://addtocart-feced-default-rtdb.asia-southeast1.firebasedatabase.app/",
+}
+
+//connecting firebase project to our project
+const app = initializeApp(appSettings)
+
+//connecting our project to firebase database
+const database = getDatabase(app)
+
+
+//creating reference in database
+const itemsInDB = ref(database, "items")
+// argument 1 - name of database in which reference is to be created
+// argument 2 - name of the refrence 
+
+
+const input_field = document.getElementById('input-field')
 const add_button=document.getElementById('add-button')
 
 add_button.addEventListener('click', function () {
+    //pushes the value in DB
+    push(itemsInDB ,input_field.value)
+    // argument 1 - reference 
+    // argument 2 - data to be pushed
+    
     alert(input_field.value)
 })
